19b900bf1799d8547b495ace4badb5aa7cf118ef
Michael Grech <mdgrech@gmail.com>
2020-02-06 12:19:53 -0500

    EOD2

--- example/main.dart
+++ example/main.dart
@@ -1,7 +1,20 @@
 import 'package:prompter_md5/src/terminal.dart';
+import 'package:prompter_md5/src/option.dart';
 
 void main() {
+  
+  
+  
+  
+  
   var terminal = new Terminal();
-  terminal.clearScreen();
-  terminal.printPrompt('Hello World!');
+
+  var options = [
+    new Option('I want red', '#f00'),
+    new Option('I want blue', '#00f')
+  ];
+
+  terminal.printOptions(options);
+  var response = terminal.collectInput();
+  print(response);
 }
--- lib/src/terminal.dart
+++ lib/src/terminal.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import './option.dart';
 
 class Terminal {
   void printPrompt(String prompt) {
@@ -11,5 +12,18 @@ class Terminal {
     } else {
       stdout.write('\x1B[2J\x1B3J\x1B[H');
     }
+  }
+
+  String collectInput() {
+    return stdin.readLineSync();
+  }
+
+  void printOptions(List<Option> options) {
+    for (var i = 0; i < options.length; i++) {
+      stdout.writeln('[$i] - ${options[i].label}');
+    }
+
+    stdout.writeln('Entr a choice\n');
+    stdout.write('>');
   }
 }
